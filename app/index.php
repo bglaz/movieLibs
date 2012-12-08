9ce7154a182b4a943f0a68d89e5b1cc57f579ae7
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2012-12-08 15:11:04 -0500

    stub router

--- app/index.php
+++ app/index.php
@@ -1,9 +1,35 @@
 <?php
 //Include a PageError class which can be used later. You supply this class.
-include('PageError.php');
-include('php-router.php');
+include('php-router/php-router.php');
 
 //Create a new instance of Router (you'd likely use a factory or container to
 // manage the instance)
 $router = new Router;
 
+//Get an instance of Dispatcher
+$dispatcher = new Dispatcher;
+$dispatcher->setSuffix('Controller');
+$dispatcher->setClassPath('controllers');
+
+//Set up a 'catch all' default route and add it to the Router.
+//You may want to set up an external file, define your routes there, and
+// and include that file in place of this code block.
+$std_route = new Route('/:class/:method/:id');
+$std_route->addDynamicElement(':class', ':class')
+          ->addDynamicElement(':method', ':method')
+          ->addDynamicElement(':id', ':id');
+$router->addRoute( 'std', $std_route );
+
+//Set up your default route:
+$default_route = new Route('/');
+$default_route->setMapClass('default')->setMapMethod('index');
+$router->addRoute( 'default', $default_route );
+
+$url = urldecode($_SERVER['REQUEST_URI']);
+
+try {
+    $found_route = $router->findRoute($url);
+    $dispatcher->dispatch( $found_route );
+} catch (Exception $e){
+    echo "you're dumb";
+} 
