d594086606629bfecf948d8241033c24f4867972
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2012-12-08 17:45:22 -0500

    remove stub

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -27,9 +27,6 @@ class home {
 
   function do_madlib() {
   	$data = $_POST['movie'];
-    $data = <<<EOF
-    {"rating":7.6,"genres":["Adventure","Western"],"rated":"PG","filming_locations":"Almer\u00eda Railway Station, Almer\u00eda, Andaluc\u00eda, Spain","language":["Italian","Spanish"],"plot":"In Mexico at the time of the Revolution, Juan, the leader of a bandit family, meets John Mallory, an IRA explosives expert on the run from the British. Seeing John's skill with explosives, Juan decides to persuade him to join the bandits in a raid on the great bank of Mesa Verde. John in the meantime has made contact with the revolutionaries, and intends to use his dynamite in their service.","runtime":["157 min","USA: 120 min (initial US release)","USA: 138 min","USA: 154 min (Laserdisc version)"],"poster":"http:\/\/ia.media-imdb.com\/images\/M\/MV5BMTY5Mjg3MzY4NV5BMl5BanBnXkFtZTcwNTM3MTc0MQ@@._V1._SY317_CR9,0,214,317_.jpg","imdb_url":"http:\/\/www.imdb.com\/title\/tt0067140\/","title":"Gi\u00f9 la testa","writers":["Sergio Leone","Sergio Donati"],"imdb_id":"tt0067140","directors":["Sergio Leone"],"rating_count":14340,"actors":["Rod Steiger","James Coburn","Romolo Valli","Maria Monti","Rik Battaglia","Franco Graziosi","Antoine Saint-John","Giulio Battiferri","Poldo Bendandi","Omar Bonaro","Roy Bosier","John Frederick","Amato Garbini","Michael Harvey","Biagio La Rocca"],"also_known_as":["A Fistful of Dynamite"],"year":1971,"country":["Italy"],"type":"M","release_date":19711020}
-EOF;
 
   	$data = json_decode($data,true);
 
