b761e00021acb55807ddf4645f9906528ea12334
Brian <brian.glaz@gmail.com>
2012-12-08 16:04:15 -0500

    get movies json

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -7,13 +7,13 @@ class home {
 
   function getMovies() {
   	if(!empty($_POST)) {
-  		$query = mysql_real_escape_string($_POST['movie_title']);
+  		$query = addslashes($_POST['movie_title']);
   		$url = "http://imdbapi.org/?title=$url&type=json&plot=full&episode=1&limit=5&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple";
 
   		$ch = curl_init($url);
   		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   		$results = json_encode(curl_exec($ch),true);
-  		pr($results);
+  		var_dump(($results));
 
   	}
   }
