880ea1b7e5708d154fb44b72a84018e805f94384
Brian <brian.glaz@gmail.com>
2012-12-08 15:53:20 -0500

    resolve conflict

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,6 +3,20 @@ class home {
   function index() {
     $tpl = newTpl();
     $tpl->display("home/index.tpl.php");
+    ;
+  }
+
+  function getMovies() {
+  	if(!empty($_POST)) {
+  		$query = mysql_real_escape_string($_POST['movie_title']);
+  		$url = "http://imdbapi.org/?title=$url&type=json&plot=full&episode=1&limit=5&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple";
+
+  		$ch = curl_init($url);
+  		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  		$results = json_encode(curl_exec($ch),true);
+  		pr($results);
+
+  	}
   }
 
 }
