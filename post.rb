2f6416a6c7ef2d1792c8da1334b014360109b572
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2012-12-08 19:58:06 -0500

    adding ruby posting script

---
+++ post.rb
@@ -0,0 +1,14 @@
+require 'tumblr_client'
+
+Tumblr.configure do |config|
+    config.consumer_key = "KuLUj5eO82pXobQxLSORvDbwAApySZU06Ai9BUUon5DMtBXT7x"
+    config.consumer_secret = "BlHl4cc4Uad3UMOKd8B9CkuYM4vq99aAtyoHyWIE4dp4XjYoSo"
+    config.oauth_token = "UqbqvDdYgFhmyhf4ACKAxm0aQBvLySGiBH46zuVHSoZ6IbGX52"
+    config.oauth_token_secret = "JlDHYu9QZZJ7dtLL04i0bPLaGCwayWLTaYJiVyv4DeWKv7mA9X"
+end
+
+
+client = Tumblr.new
+photo_url = ARGV[0]
+caption = ARGV[1]
+puts client.photo("movielibs.tumblr.com", :source => photo_url, :caption => caption)
