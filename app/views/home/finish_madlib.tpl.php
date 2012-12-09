4d27224e89b7c21a6d96ab0fac94160f726bf4a4
Brian <brian.glaz@gmail.com>
2012-12-08 20:40:33 -0500

    is it finished?

--- app/views/home/finish_madlib.tpl.php
+++ app/views/home/finish_madlib.tpl.php
@@ -16,4 +16,30 @@
 		<?=stripslashes($this->final_story);?>
 	</div>
 </div>
+
+<input style="margin: 10px 0px;" id="post_btn" type="submit" class="btn btn-info btn-large" value="Post to Tumblr!" />
+
+<form id="myForm">
+	<input type="hidden" name="story" value="<?=strip_tags($this->final_story);?>" />
+	<input type="hidden" name="img" value="<?=$this->img;?>" />
+</form>
+
+<script type="text/javascript">
+	$(function() {
+		$('#post_btn').click(function() {
+			$.ajax({
+				url: 	"/post_madlib",
+				type: 	"POST",
+				data: 	$('#myForm').serialize(),
+				success: 	function() {
+					alert('Your madlib is now live on movelibs.tumblr.com !');
+				},
+				error: 	function() {
+					alert('Oops... Something went wrong. Please try again.')
+				}
+			});
+		});
+	});
+</script>
+
 <?php echo $this->footer; ?>
